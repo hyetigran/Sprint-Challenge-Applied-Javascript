28bf2e864610c6a9ba59146c357b0432a998d89a
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-05-17 14:08:03 +0400

    start carousel, incomplete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,8 @@
-class Carousel {
+// class Carousel {
 
-}
+// }
 
-let carousel = document.querySelector();
+// let carousel = document.querySelector();
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
@@ -12,3 +12,17 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+!function(doc) {
+	let imageList = 'img';
+	(images = doc.querySelect(imageList)), (totalImages = image.length), (slide = 0), (moving = true);
+
+	function setInitialClasses() {
+		items[totalImages - 1].classList.add('left-button');
+		items[0].classList.add('active');
+		items[1].classList.add('rigth-button');
+	}
+	function setEventListeners() {
+		let next = doc.get;
+	}
+};
